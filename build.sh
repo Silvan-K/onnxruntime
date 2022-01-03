cef202647f31d515923511659c112e57bd4cd557
Silvan Kuttimalai <silvan.kuttimalai@1qbit.com>
2022-01-03 13:54:48 -0800

    Adapt build script for groq

--- build.sh
+++ build.sh
@@ -18,5 +18,12 @@ elif [[ "$*" == *"--android"* ]]; then
     DIR_OS="Android"
 fi
 
+# Build and create python wheel
+SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
+bake run -t //Groq/Tools:hempy -- hempy $DIR/tools/ci_build/build.py --build_dir $DIR/build/$DIR_OS  --parallel=6 --build_wheel --config RelWithDebInfo --build_shared_lib --skip_tests
+cd build/Linux/RelWithDebInfo && bake run -t //Groq/Tools:hempy -- hempy ../../../setup.py bdist_wheel
 
-bake run -t //Groq/Tools:hempy -- hempy $DIR/tools/ci_build/build.py --build_dir $DIR/build/$DIR_OS "$@"
+# Upload wheel and modify path to wheel in nix file
+ORT_NIX_PATH=$(nix add-to-store $(find ./ -name "onnxruntime*.whl"))
+bake upload $ORT_NIX_PATH
+sed -i 's%/nix.*whl%'"$ORT_NIX_PATH"'%' $(git rev-parse --show-toplevel)/bake/nix/global/uncached-full/groqpkgs/py/onnxruntime-whl.nix 
